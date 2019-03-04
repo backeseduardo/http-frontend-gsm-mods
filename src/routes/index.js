13244f0a7754089ddfae2ab47f8bc3c18981e39b
Eduardo Backes <e.backes2011@gmail.com>
2019-03-04 17:29:41 -0300

    Send data with post

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
+  // console.log('Time: ', Date.now())
   next()
 })
 // define the home page route
